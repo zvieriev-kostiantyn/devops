57a532f1b32e6bbfb9f95fc6b27ab5c54c6b0df4
Kostya <zverevfpm@gmail.com>
2022-12-22 21:49:17 +0200

    *: add jenkins plugin

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#docker build -t base-jenkins ./base_image
+docker build -t base-jenkins ./base_image
 
 docker build -t master-jenkins ./master_image && docker run --name jenkins --rm -dp 8080:8080 --env JENKINS_ADMIN_ID=admin --env JENKINS_ADMIN_PASSWORD=password master-jenkins
--- master_image/utilities/seedJobs.groovy
+++ master_image/utilities/seedJobs.groovy
@@ -1,32 +1,3 @@
-package javaposse.jobdsl.plugin
-
-import hudson.Extension
-import jenkins.model.GlobalConfiguration
-import jenkins.model.GlobalConfigurationCategory
-import net.sf.json.JSONObject
-import org.kohsuke.stapler.StaplerRequest
-
-@Extension
-class GlobalJobDslSecurityConfiguration extends GlobalConfiguration {
-
-    GlobalConfigurationCategory getCategory() {
-        GlobalConfigurationCategory.get(GlobalConfigurationCategory.Security)
-    }
-
-    boolean useScriptSecurity = true
-
-    GlobalJobDslSecurityConfiguration() {
-        load()
-    }
-
-    @Override
-    boolean configure(StaplerRequest req, JSONObject json) {
-        useScriptSecurity = json.has('useScriptSecurity')
-        save()
-        true
-    }
-}
-
 def gitType = System.getenv('ENVIRONMENT') == 'prod' ? 'PT_TAG' : 'PT_BRANCH_TAG'
 
 def folderName = 'DevOps'
